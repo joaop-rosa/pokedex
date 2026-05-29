ecc25bb011ac7f44e6fb5273f586e6c10ff9c6f8
joao.rosa <jp.ragdool@hotmail.com>
2026-05-28 21:07:34 -0300

    addded env variables

--- src/contants/socket.ts
+++ src/contants/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_API_URL || "http://localhost:3003";
+const URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 export const socket = io(URL, {
 	autoConnect: false,
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
 import path from "node:path";
 import generouted from "@generouted/react-router/plugin";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-	plugins: [react(), svgr(), generouted()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./"),
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		plugins: [react(), svgr(), generouted()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./"),
+			},
 		},
-	},
-	server: {
-		port: 3000,
-	},
+		server: {
+			port: parseInt(env.PORT || env.VITE_PORT || "3000"),
+		},
+	};
 });
